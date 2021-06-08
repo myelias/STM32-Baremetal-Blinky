fd9c1b45f73a29176c15b3974d51d6f5e10d47d8
Martin Elias <72317416+myelias@users.noreply.github.com>
2021-06-07 21:39:50 -0500

    Update main_blinky_baremetal.cpp

--- main_blinky_baremetal.cpp
+++ main_blinky_baremetal.cpp
@@ -12,42 +12,40 @@
 //DigitalOut led1(LED1);
 
 //Refer to STM32 reference manual for all register information
+
 int main()
 {   
     unsigned int i, j;
-    RCC->CR &= 0x00000000;
-    RCC->CR |= (1<<16);
-    while(!(RCC->CR)&(1<<17));
-    RCC->CFGR &= 0x00000000;
-    RCC->CFGR |= 0xA0;          //Clock frequency is decreased and delay will increase by factor of 8
-    RCC->AHB1ENR &= 0x00000000;
-    RCC->AHB1ENR |= (1<<0);     //was (1<<0)
+    RCC->CR &= 0x00000000;      //Reset Clock Control Register
+    RCC->CR |= (1<<16);         //Enable High Speed External (HSE) Clock Signal
+    while(!(RCC->CR)&(1<<17));  //the 17th bit in the RCC_CR indicates if the HSE is ready, so we wait in a while loop until it is true.
+    RCC->CFGR &= 0x00000000;    //Reset Clock Configuration Register
+    RCC->CFGR |= 0xA0;          //Now, we are setting the AHB1 prescaler. We set it to 1010 0000 to divide the system clock by 8.
+    RCC->AHB1ENR &= 0x00000000; //GPIOA is connected to the AHB1 bus. So we must reset the bus then enable the clock to the GPIOA register.
+    RCC->AHB1ENR |= (1<<0);     //Enable GPIOA within the RCC_AHB1ENR1 Register
 
-    GPIOA->MODER |= 0xA8000000; 
-    GPIOA->MODER |= (1<<10);
-    GPIOA->OTYPER |= 0x00;
-    GPIOA->OSPEEDR |=(1<<10);
-    GPIOA->PUPDR = 0x64000000;
-    GPIOA->PUPDR |= 0x00;
-    GPIOA->ODR &= 0x00000000; //might not need this line of code
+    //Configure the desired I/O as output or input in the GPIOx_MODER Register
 
-    //printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+    GPIOA->MODER |= 0xA8000000; //Use this reset value to reset Port A mode register
+    GPIOA->MODER |= (1<<10);    //Enable General Purpose Output Mode (01) on pins 11 and 10 to enable pin 5 on Port A (PA5)
+    GPIOA->OTYPER |= 0x00;      //Reset GPIO output type for Port A
+    GPIOA->OSPEEDR |=(1<<10);   //Set Port A Pin 5 speed to Medium Speed (01) (Bits 11 and 10)
+    GPIOA->PUPDR = 0x64000000;  //Reset value for Port A port pull-up/pull-down register
+    GPIOA->PUPDR |= 0x00;       //Set Port A PUPDR as No pull-up or pull-down (00)
+    GPIOA->ODR &= 0x00000000;   //Reset Port A Output Data Register
+
 
     while (1)
     {
-
-        GPIOA->ODR = (1<<5);
+        //Enabling the ODR turns on LED and disabling the ODR turns off the LED. Turning the ODR register on and off achieves a blinking effect.
+        GPIOA->ODR = (1<<5);    //Enable Output Data Register on Port A Pin 5
         for(i=0; i<1000000; i++){
-            j = j + 0.1;    //check to see of compiler will optimize empty loop with nothing occurring here (i.e. loop body might not be needed)
+            j = j + 0.1;        //check to see of compiler will optimize empty loop with nothing occurring here (i.e. loop body might not be needed)
         }
-        GPIOA->ODR = (0<<5);
+        GPIOA->ODR = (0<<5);    //Disable Output Data Register on Port A Pin 5
         for(i=0; i<1000000; i++){
             j = j + 0.1;
         }
 
-
-        //GPIOA->ODR ^= (1<<5);
-        //led1 = !led1;
-        //thread_sleep_for(WAIT_TIME_MS);
     }
 }
